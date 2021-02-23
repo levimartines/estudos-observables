95c6f63207578ca72d290ea4768f877957b3f5c6
levimartines <levi.mferreira@gmail.com>
2021-02-23 13:58:49 -0300

    Hot Observables

--- src/app/hot-observables/hot-observables.component.ts
+++ src/app/hot-observables/hot-observables.component.ts
@@ -6,49 +6,52 @@ import { fromEvent, Observable } from 'rxjs';
   templateUrl: './hot-observables.component.html',
   styleUrls: ['./hot-observables.component.css']
 })
-export class HotObservablesComponent implements OnInit {
+export class HotObservablesComponent implements OnInit, OnDestroy {
   @ViewChild('myButton', {static: true}) button?: ElementRef;
   number1 = 0;
   number2 = 0;
   string1 = 'Initialized';
   string2 = 'Wait for button click';
+  p = new Producer();
 
   constructor() {
   }
 
   ngOnInit(): void {
-    class Producer {
-      private myListeners: any[] = [];
-      private n = 0;
-      private id: any;
-
-      addListener(lister: any): void {
-        this.myListeners.push(lister);
-      }
-
-      start(): void {
-        this.id = setInterval(() => {
-          this.n++;
-          this.myListeners.forEach(lister => lister(this.n));
-        }, 1000);
-      }
-
-      stop(): void {
-        clearInterval(this.id);
-      }
-    }
-
-    const p = new Producer();
-    p.start();
-    p.addListener((next: any) => this.number1 = next);
+    this.p.start();
+    this.p.addListener((next: any) => this.number1 = next);
 
     console.log(this.button);
 
     const myBtnClickObservable: Observable<any> = fromEvent(this.button?.nativeElement, 'click');
     myBtnClickObservable.subscribe(() => {
       this.string2 = 'Initialized';
-      p.addListener((next: any) => this.number2 = next);
+      this.p.addListener((next: any) => this.number2 = next);
     });
   }
 
+  ngOnDestroy(): void {
+    this.p.stop();
+  }
+}
+
+class Producer {
+  private myListeners: any[] = [];
+  private n = 0;
+  private id: any;
+
+  addListener(lister: any): void {
+    this.myListeners.push(lister);
+  }
+
+  start(): void {
+    this.id = setInterval(() => {
+      this.n++;
+      this.myListeners.forEach(lister => lister(this.n));
+    }, 500);
+  }
+
+  stop(): void {
+    clearInterval(this.id);
+  }
 }
